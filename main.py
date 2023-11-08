beae60709cd3bbdce131ce63c695c6a6cda4fa5d
Luiz CVBSJ <luizcvbsj@gmail.com>
2023-11-08 22:26:07 +0100

    added interaction from the class to the cep search

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 from cep_access import AdressSearch
 
-cep1 = AdressSearch("11696036")
-
-print(cep1)
-
-r = requests.get("https://viacep.com.br/ws/01001000/json")
+cep1 = AdressSearch("11685402")
+request = "https://viacep.com.br/ws/{}/json".format(cep1)
+r = requests.get(request)
 
 print(r)
+print(r.text)
